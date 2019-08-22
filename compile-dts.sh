c08a31d3e06e6f3a36aa9d70dc2f697516ee7c2a
Tarek Soliman <tarek@bashasoliman.com>
2019-08-22 07:44:32 -0500

    Silence unit_address_vs_reg warning
    The kernel makefiles add "-W no-unit_address_vs_reg" to the dtc command line

--- compile-dts.sh
+++ compile-dts.sh
@@ -4,5 +4,5 @@ for DTS_FILE in *-overlay.dts
 do
     DTBO_FILE=${DTS_FILE/-overlay.dts/.dtbo}
     rm -f $DTBO_FILE
-    dtc -I dts -O dtb -o "$DTBO_FILE" $DTS_FILE
+    dtc -W no-unit_address_vs_reg -I dts -O dtb -o "$DTBO_FILE" $DTS_FILE
 done
